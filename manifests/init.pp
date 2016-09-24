c475503d6f8e3585d130a95bb56ff048ef70af20
root <root@binddns.puppetlearning.local>
2016-09-24 05:16:14 -0400

    Adding param class and modifications to resolver class

---
+++ manifests/init.pp
@@ -0,0 +1,13 @@
+class resolver (
+	$search = resolver::params::search,
+	$nameservers = resolver::params::nameserver
+) inherits resolver::params {
+
+file { '/etc/resolv.conf':
+     ensure  => file,
+     owner   => 'root',
+     group   => 'root',
+     mode    => '0644',
+     content => template('resolver/resolv.conf.erb'),
+   }
+ }
---
+++ manifests/params.pp
@@ -0,0 +1,7 @@
+class resolver::params {
+    $search = 'puppetlearning.local'
+    $nameservers = [
+	'8.8.8.8',
+	'4.2.2.2',
+	]
+}
